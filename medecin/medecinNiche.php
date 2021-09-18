9ef0ae271f2c9d88de480be2aafc1981ebab9056
Axel VISSOH <ayitchededji.vissoh@groupe-esigelec.org>
2021-09-18 20:19:02 +0200

    Toutes les pages pour le medecin sans css

---
+++ medecin/medecinNiche.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="medecin.css">
+    <link rel="stylesheet" href="navbarMedecin.css">   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Medecin</title>
+</head>
+<body>
+    <!--header-->
+    <?php include("../medecin/navbarMedecin.php")?>
+    <!---end header-->
+    <?php
+        echo 'Welcome Back '.$_SESSION['surname'].' '.$_SESSION['name'].'.';
+    ?>
+    <!--footer-->
+    <?php include("../home/footer.php")?>
+    <!---end footer-->
+</body>
+</html>
---
+++ medecin/navbarMedecin.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="#">
+        <img src="../home/esigLogo.jpg" alt="The logo of esigelec" style="width:40px;">
+        <p>Esig'vaccination</p>
+    </a>    
+    <!-- Links -->
+    <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="#">Niche</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Certificates</a></li>
+        <li class="nav-item"><a class="nav-link" href="../log/logoff.php">Log off</a></li>
+    </ul>
+</nav>
