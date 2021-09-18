7a2af72348eadf8211412bd1b8d0b01234b63abe
Jédielle HOUNSONLON <detondji.hounsonlon@groupe-esigelec.org>
2021-09-18 12:30:08 +0200

    Script du footer de la page d'acceuil

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Footer</title>
+</head>
+<body>
+   
+    <div class=" ml-5 container-fluid fixed-bottom bg-light">
+        <footer class="row row-cols-3 py-2 my-2">
+          <div class="col">
+            <h5>Copyright &copy; esigelec 2021</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"> Créer par esigelec </li>
+            </ul>
+          </div>
+      
+          <div class="col">
+            <h5>Nous contacter</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">Mail: esigelec.irseem@yahoo.fr </li>
+              <li class="nav-item mb-2">Numéro de Tel : + (0) 2 55 75 95 86</li>
+            </ul>
+          </div>
+      
+          <div class="col">
+            <h5>Découvrir Esigelec</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Qui sommes-nous ?</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">D'où venons nous ?</a></li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</html>    
