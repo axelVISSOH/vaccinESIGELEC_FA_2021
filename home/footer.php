7428809a45e8823775b9bfa0736566b4800e31ba
Jédielle HOUNSONLON <detondji.hounsonlon@groupe-esigelec.org>
2021-09-21 14:50:10 +0200

    Nouvelle version du dossier home

--- home/footer.php
+++ home/footer.php
@@ -1,28 +1,19 @@
-<footer class="row row-cols-3 py-2 my-2 ml-5 container-fluid fixed-bottom bg-light">
-      <div class="col">
+<footer class="row  fixed-bottom">
+      <div class="col-sm-4">
         <h5>Copyright &copy; esigelec 2021</h5>
         <ul class="nav flex-column">
             <li class="nav-item mb-2"> Créer par les étudiants de l'Esigelec </li>
         </ul>
       </div>      
-      <div class="col">
+      <div class="col-sm-4">
         <h5>Nous contacter</h5>
         <ul class="nav flex-column">
           <li class="nav-item mb-2">Mail: esigelec.irseem@yahoo.fr </li>
           <li class="nav-item mb-2">Numéro de Tel : + (0) 2 55 75 95 86</li>
         </ul>
       </div>      
-      <div class="col">
+      <div class="col-sm-4">
           <h5>Découvrir Esigelec</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="https://www.esigelec.fr/fr" class="nav-link p-0 text-muted">Qui sommes-nous ?</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">D'où venons nous ?</a></li>
-          </ul>
+          <a href="https://www.esigelec.fr/fr">Qui sommes-nous ?</a>
       </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
-integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
-integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- home/index.php
+++ home/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">    
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="../home/home.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -13,69 +13,32 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <title>Home</title>
+   <title>Home</title>
 </head>
 <body>
-    <!--header-->
-    <?php include("../home/navbarHome.php");?>
-    <!--end header-->
-    <!--section-->
-    <section>
-        <div class="container">
-            <div class="row info">
-                <div class="col">
-                    <img src="vacinationcenter.png" alt="A vaccination center picture">
-                </div>
-                <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error blanditiis ducimus! Cupiditate
-                        pariatur quia
-                        tenetur maxime optio soluta ab iusto hic consequuntur delectus itaque minus, deleniti, magnam voluptatum
-                        voluptatem.
-                    </p>
-                </div>
+        <!--header-->
+        <?php include("../home/navbarHome.php");?>
+        <!--end header-->  
+        <div class="row">
+            <div class="col-lg-7 left">
+                <h1>Welcome to Esig'Vaccination.</h1>
+                <img src="vacinationcenter.png" alt="A vaccination center picture">
             </div>
+            <div class="col-lg-4 right"> 
+                <h4 class="info text-center">Informations</h4>
+                <p>
+                    Dear students,</p>
+                <p>
+                    Due to the current health context and concerned about your well-being, ESIGELEC is setting up a small vaccination center within the establishment.
+                    You will therefore be able to make an appointment online and get vaccinated during your off-peak hours.
+                    Once a dose has been assigned, you will receive your certificate in your personal space.
+                    The site is in its genesis do not hesitate to report problems to technical support.
+                </p>
+            </div>            
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error blanditiis ducimus! Cupiditate
-                        pariatur quia
-                        tenetur maxime optio soluta ab iusto hic consequuntur delectus itaque minus, deleniti, magnam voluptatum
-                        voluptatem.
-                    </p>
-                </div>
-                <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error blanditiis ducimus! Cupiditate
-                        pariatur quia
-                        tenetur maxime optio soluta ab iusto hic consequuntur delectus itaque minus, deleniti, magnam voluptatum
-                        voluptatem.
-                    </p>
-                </div>
-                <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error blanditiis ducimus! Cupiditate
-                        pariatur quia
-                        tenetur maxime optio soluta ab iusto hic consequuntur delectus itaque minus, deleniti, magnam voluptatum
-                        voluptatem.
-                    </p>
-                </div>
-                <div class="col">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error blanditiis ducimus! Cupiditate
-                        pariatur quia
-                        tenetur maxime optio soluta ab iusto hic consequuntur delectus itaque minus, deleniti, magnam voluptatum
-                        voluptatem.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section> 
-    <!--footer-->
-    <?php include("../home/footer.php")?>
-    <!--end footer-->
+        <p class="infoT">Thrust Us !!!</p>          
+        <!--footer-->
+        <?php include("../home/footer.php")?>
+        <!--end footer-->        
 </body>
 </html>
