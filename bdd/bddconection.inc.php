1c0793970f7ceaa165a5b8ff99f8abf8f38f9b17
axelVISSOH <ayitchededji.vissoh@groupe-esigelec.org>
2021-09-21 15:01:05 +0000

    Delete bddconection.inc.php

--- bdd/bddconection.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-    $bdd = new PDO('mysql:host=localhost;dbname=sitevaccin;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}catch(Exception $e){ 
-    die('Error:'.$e->getMessage());
-}
-?>
