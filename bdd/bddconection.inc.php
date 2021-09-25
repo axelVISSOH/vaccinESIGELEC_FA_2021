4727613450c7f651f2d5ab74b12d609c7ac3add3
jedags02 <detondji.hounsonlon@groupe-esigelec.org>
2021-09-25 22:00:09 +0000

    Delete bddconection.inc.php

--- bdd/bddconection.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=sitevaccin;charset=utf8','root','Doublefuck24',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));//for me
-        //$bdd = new PDO('mysql:host=localhost;dbname=bdd_16_4;charset=utf8','grp_16_4','Deeshu4Ai8',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));//for school
-    }catch(Exception $e){ 
-        die('Error:'.$e->getMessage());
-    }
-?>
